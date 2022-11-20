8deba663003af5413ebe9b931661608cc149e4f5
Dhruvik <dhruviksparikh@gmail.com>
2022-11-20 23:31:25 +0530

    Added changes

---
+++ src/main/java/com/simplilearn/controller/MovieController.java
@@ -0,0 +1,37 @@
+package com.simplilearn.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.simplilearn.entity.Movie;
+import com.simplilearn.service.MovieService;
+
+@RestController
+public class MovieController {
+
+	@Autowired
+	MovieService movieService;
+
+	@GetMapping("/movies")
+	public List<Movie> getAllMovies() {
+		return movieService.getMovies();
+	}
+
+	@PostMapping("/movies")
+	public int saveMove(@RequestBody Movie movie) {
+		movieService.saveMovie(movie);
+		return movie.getId();
+	}
+	
+	@DeleteMapping("/movies/{id}")
+	public void deleteMovie(@PathVariable("id") int id) {
+		movieService.delete(id);
+	}
+}
